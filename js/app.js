5f745ca19c4f4677f86fd295131077c7f192a387
Harshil Kothari <harshilkothari1995@gmail.com>
2020-06-09 20:52:10 -0400

    change

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   $('.header').height($(window).height());
 })
 $(document).ready(function () {
+  $('#about-header').height($(window).height());
+})
+$(document).ready(function () {
   $('.cardio-header').height($(window).height());
 })
 $(document).ready(function () {
